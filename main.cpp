ecbf7da43147ad96f5c449827d7f72f8f8f89df4
EdvinLiu <edwinlub132@gmail.com>
2024-12-29 13:39:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <cctype>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
@@ -25,18 +26,20 @@ int main() {
         return 1;
     }
 
-    // Asociatyvus konteineris skaičiuoti žodžių pasikartojimus
-    map<string, int> wordCount;
+    // Asociatyvus konteineris skaičiuoti žodžių pasikartojimus ir jų vietas
+    map<string, vector<int>> wordOccurrences;
     string line;
+    int lineNumber = 0;
 
     // Nuskaitome failo eilutes ir skaičiuojame žodžius
     while (getline(inputFile, line)) {
+        lineNumber++;
         stringstream ss(line);
         string word;
         while (ss >> word) {
             word = removePunctuation(word);
             if (!word.empty()) {
-                wordCount[word]++;
+                wordOccurrences[word].push_back(lineNumber);
             }
         }
     }
@@ -50,9 +53,15 @@ int main() {
         return 1;
     }
 
-    for (const auto &entry : wordCount) {
-        if (entry.second > 1) {
-            outputFile << entry.first << " " << entry.second << endl;
+    for (const auto& entry : wordOccurrences) {
+        if (entry.second.size() > 1) {
+            outputFile << entry.first << " pasikartoja " << entry.second.size() << " kartus, eilutese: ";
+            for (size_t i = 0; i < entry.second.size(); i++) {
+                outputFile << entry.second[i];
+                if (i < entry.second.size() - 1) outputFile << ", ";
+            }
+            outputFile << endl;
+            outputFile << endl;
         }
     }
 
@@ -61,3 +70,4 @@ int main() {
     cout << "Programa baigta. Rezultatai issaugoti faile rezultatai.txt." << endl;
     return 0;
 }
+
